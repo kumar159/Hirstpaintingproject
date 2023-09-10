618fca0ce425f21f01b80469f062f74e53368dfb
kumar159 <140960855+kumar159@users.noreply.github.com>
2023-09-10 20:55:32 +0530

    Add files via upload

---
+++ hirst_2.py
@@ -0,0 +1,50 @@
+import turtle
+from turtle import Turtle, Screen
+import random
+
+turtle.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    my_tuple = (r, g, b)
+    return my_tuple
+
+
+tim = Turtle()
+tim.speed("fastest")
+
+tim.penup()
+tim.hideturtle()
+
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random_color())
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
